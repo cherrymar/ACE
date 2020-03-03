e343481602cb704a3d8253baaad4e2c1b100cbd3
zoomClinic1920 <laptop19@laptop19s-MacBook-Pro.local>
2020-03-03 12:35:46 -0800

    feat(food info): previous commit did not add file content. The food by ID lookup should be functioning but is not showing up visually on the app.

--- Ace/FoodStructure.swift
+++ Ace/FoodStructure.swift
@@ -1,9 +1,62 @@
 //
 //  FoodStructure.swift
-//  Ace
+//  RestManager
 //
 //  Created by zoomClinic1920 on 3/3/20.
-//  Copyright © 2020 Cher Ma . All rights reserved.
+//  Copyright © 2020 Appcoda. All rights reserved.
 //
 
 import Foundation
+
+var defString = String(stringLiteral: "")
+var defInt = -1
+
+struct FoodData: Codable, CustomStringConvertible {
+    var desc: String?
+    var category: foodCategory?
+    var foodClass: String = "no"
+    var foodNutrients: String = "no"
+    var foodComponents: String = "no"
+    var scientificName: String = "no"
+    var foodAttributes: String = "no"
+    var tableAliasName: String = "no"
+    var nutrientConversionFactors: String = "no"
+    var isHistoricalReference: String = "no"
+    var ndbNumber: String = "no"
+    var footnote: String = "no"
+    var fdcId: String = "no"
+    var dataType: String = "no"
+    var publicationDate: String = "no"
+    var foodPortions: String = "no"
+    var inputFoods: String = "no"
+    var foodUpdateLog: String = "no"
+    var changes: String = "no"
+    
+    private enum CodingKeys: String, CodingKey {
+        case desc = "description"
+        case category = "foodCategory"
+    }
+    
+    var description: String {
+        let output = """
+        description = \(desc ?? defString)
+        category = \(category?.description ?? defString)
+        
+        """
+        
+        return output
+    }
+}
+
+struct foodCategory: Codable {
+    var id: Int
+    var code: String
+    var description: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case code = "code"
+        case description = "description"
+    }
+}
+
--- Ace/RestCaller.swift
+++ Ace/RestCaller.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+class RestCaller {
+    let rest = RestManager()
+    let apiKey = "RH3s29lFBdgsmpyxvQAwGJz60Ec71bxVlgjUBfRX"
+    func getFood(foodId: String) -> String {
+        let baseUrl = "https://api.nal.usda.gov/fdc/v1/"
+
+        let fullUrl = baseUrl + foodId + "?api_key=" + apiKey
+        var foodInfo = ""
+        guard let url = URL(string: fullUrl) else { return defString}
+        
+        rest.requestHttpHeaders.add(value: "application/json", forKey: "Content-Type")
+        rest.makeRequest(toURL: url, withHttpMethod: .get) { (results) in
+            if let data = results.data {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                guard let userData = try? decoder.decode(FoodData.self, from: data) else { return }
+                foodInfo = (userData.description)
+            }
+        }
+        return foodInfo
+    }
+}
