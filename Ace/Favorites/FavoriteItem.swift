556b2f41446bcefed45653d652b508f90fca5434
Cher Ma <cherma88@hotmail.com>
2020-04-02 17:26:38 -0700

    fix= need to fix bugs in code still

--- Ace/Favorites/FavoriteItem.swift
+++ Ace/Favorites/FavoriteItem.swift
@@ -9,10 +9,28 @@
 import Foundation
 import CoreData
 
+import SwiftUI
+import UIKit
+
 public class FavoriteItem:NSManagedObject, Identifiable {
     @NSManaged public var link: String?
     @NSManaged public var prepTime: Double
     @NSManaged public var recipeName: String?
+    
+    func inFavorites(checkLink: String) -> Bool {
+        // Get favorites from database
+        let request:NSFetchRequest<FavoriteItem> = FavoriteItem.fetchRequest() as!
+            NSFetchRequest<FavoriteItem>
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let managedObjectContext = appDelegate.persistentContainer.viewContext
+        
+        request.predicate = NSPredicate(format: "link == %@", checkLink)
+        
+        
+        return request.fetchBatchSize != 0
+    }
 }
 
 extension FavoriteItem {
@@ -28,30 +46,18 @@ extension FavoriteItem {
         return request
     }
     
-    static func inFavorites(link: String) -> Bool {
-        // Get favorites from database
-//        fetchRequest.predicate = NSPredicate(format: "link == %@", link)
-        
-        let request:NSFetchRequest<FavoriteItem> = FavoriteItem.fetchRequest() as!
-            NSFetchRequest<FavoriteItem>
-
-        // Sort list of ingredients by time added
-        let sortDescriptor = NSSortDescriptor(key: "link", ascending: true)
-        request.sortDescriptors = [sortDescriptor]
-
-        return true
-    }
     
-    static func deleteFromFavorites(type: String) -> NSFetchRequest<FavoriteItem> {
-        // Get favorites from database
-        let request:NSFetchRequest<FavoriteItem> = FavoriteItem.fetchRequest() as!
-            NSFetchRequest<FavoriteItem>
-
-        // Sort list of ingredients by time added
-        let sortDescriptor = NSSortDescriptor(key: type, ascending: true)
-        request.sortDescriptors = [sortDescriptor]
-
-        return request
-    }
+//
+//    static func deleteFromFavorites(type: String) -> NSFetchRequest<FavoriteItem> {
+//        // Get favorites from database
+//        let request:NSFetchRequest<FavoriteItem> = FavoriteItem.fetchRequest() as!
+//            NSFetchRequest<FavoriteItem>
+//
+//        // Sort list of ingredients by time added
+//        let sortDescriptor = NSSortDescriptor(key: type, ascending: true)
+//        request.sortDescriptors = [sortDescriptor]
+//
+//        return request
+//    }
 }
 
---
+++ Ace/Recipe.swift
@@ -0,0 +1,9 @@
+//
+//  Recipe.swift
+//  Ace
+//
+//  Created by Cher Ma  on 3/31/20.
+//  Copyright © 2020 Cher Ma . All rights reserved.
+//
+
+import Foundation
